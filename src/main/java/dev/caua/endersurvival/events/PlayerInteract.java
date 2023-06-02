901af5f1250f55e32870835368c13b7209aaed74
needev <cauamelo2345@gmail.com>
2023-06-02 17:23:35 -0300

    fix nullpointer

--- src/main/java/dev/caua/endersurvival/events/PlayerInteract.java
+++ src/main/java/dev/caua/endersurvival/events/PlayerInteract.java
@@ -1,5 +1,6 @@
 package dev.caua.endersurvival.events;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -12,10 +13,14 @@ public class PlayerInteract implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onInteract(PlayerInteractEvent e) {
+        if (e.getClickedBlock() == null) return;
+        if (e.getItem() == null) return;
         if (Objects.requireNonNull(e.getClickedBlock()).getBlockData().getMaterial() != Material.SPAWNER) return;
-        if (!Objects.requireNonNull(Objects.requireNonNull(e.getItem()).getItemMeta()).getDisplayName().contains("Ball")) return;
-        e.setCancelled(true);
-        e.getPlayer().sendMessage("§cVocê não pode alterar o tipo do spawner usando pokébolas.");
+        if (Objects.requireNonNull(Objects.requireNonNull(e.getItem()).getItemMeta()).getDisplayName().contains("Ball") || Objects.requireNonNull(e.getItem().getItemMeta().getLore()).get(0).contains("Tipo:")) {
+            Bukkit.getConsoleSender().sendMessage(Objects.requireNonNull(e.getItem().getItemMeta().getLore()).get(0));
+            e.setCancelled(true);
+            e.getPlayer().sendMessage("§cVocê não pode alterar o tipo do spawner usando pokébolas.");
+        }
     }
 
 }
